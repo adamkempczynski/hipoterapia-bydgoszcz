2d5ce586c17ad3837fd03299f6285802193ea48a
Adam <admin@kermed.pl>
2025-07-08 23:29:33 +0200

    Dodanie treści, podstron i systemu aktualności

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,12 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    }
+  }
 });
